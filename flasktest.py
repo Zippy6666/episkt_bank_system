176cd13142aeacf6237563fb3a1e311bb0af7c35
Zippy6666 <simperzippy@gmail.com>
2024-01-03 12:28:19 +0100

    flaska

---
+++ flasktest.py
@@ -0,0 +1,15 @@
+import webbrowser
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('debank.html')
+
+# Launch the web browser before running the app
+webbrowser.open("http://127.0.0.1:5000/")
+
+# Run the app with the specified host and port
+app.run("127.0.0.1", port=5000)
