a47ac74cc51922d55feda7e0abe19d64f9c9d52f
Zippy6666 <simperzippy@gmail.com>
2024-01-08 10:53:20 +0100

    spaces

--- flasktest.py
+++ flasktest.py
@@ -1,6 +1,7 @@
 import webbrowser
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
 
@@ -8,8 +9,10 @@ app = Flask(__name__)
 def index():
     return "amogus gaming" # render_template('debank.html')
 
+
 # Launch the web browser before running the app
 webbrowser.open("http://127.0.0.1:5000/")
+
 
 # Run the app with the specified host and port
 app.run("127.0.0.1", port=5000)
