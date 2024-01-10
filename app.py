7246785b7893e2fd361359ed98d97e3b56f543fc
Zippy6666 <simperzippy@gmail.com>
2024-01-10 16:05:34 +0100

    venne

--- app.py
+++ app.py
@@ -1,31 +1,33 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-
+from models import db # db stands for database ofc
 
 
 """In Flask, migrating refers to the process of applying changes to your database schema using a tool like Flask-Migrate in
 conjunction with SQLAlchemy. When you develop a web application, your database schema might evolve as you add new features,
 modify existing ones, or refactor your models."""
+
+"""In the context of databases,
+a migration refers to the management and application of changes to the structure of the database schema over time.
+It's a way to version-control and apply alterations to the database structure without losing data."""
 from flask_migrate import Migrate, upgrade
 
 
 
-from models import db
- 
-
-
  
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:password@localhost/lennartbladhfuktig'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:my-secret-pw@localhost:3306/bnk'
  
+
 db.app = app
 db.init_app(app)
+
+
+
+
 migrate = Migrate(app, db)
  
 
-def seed_data():
-    """Don't do this every run or you will get more and more data the more you run it"""
-    pass
 
 
 """In Flask, the index function is typically associated with the root URL /.
@@ -34,12 +36,13 @@ Flask uses routing, allowing you to define different functions for different URL
 The index function often serves as the entry point or the landing page of your website."""
 @app.route('/')
 def index():
-    return "Hej"
+    return render_template('index.html')
  
  
 if __name__ == '__main__':
     with app.app_context():
         # Database upgrade
+        # db.create_all()
         upgrade()
    
-    app.run(debug=True)
+    #app.run(debug=True)
