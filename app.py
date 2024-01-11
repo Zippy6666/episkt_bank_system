c04226170ca74289640f1e795cd72c2e8813a21a
Zippy6666 <simperzippy@gmail.com>
2024-01-11 16:13:43 +0100

    free template

--- app.py
+++ app.py
@@ -1,52 +1,44 @@
 # imports
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from models import db, Customer, seed_data
+from models import db, Customer
 import webbrowser
 from flask_migrate import Migrate, upgrade
 
 
-
 # app
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:my-secret-pw@localhost:3306/bnk'
- 
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://root:my-secret-pw@localhost:3306/bnk"
+
 
 # db
 db.app = app
 db.init_app(app)
 
 
-
 # migrate
 migrate = Migrate(app, db)
- 
 
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/customers')
+@app.route("/customers")
 def customers():
-    all_customers = Customer.query.all() # Returns a python list of customers
+    all_customers = Customer.query.all()  # Returns a python list of customers
+    print(all_customers)
+
 
 
 def main():
-    
-
-
-    # flask db migrate -m "description_of_changes"
-    # flask db upgrade
     with app.app_context():
-        seed_data()
-        #upgrade()
+        upgrade()
+
+    webbrowser.open("http://127.0.0.1:5000/")
+    app.run("127.0.0.1", port=5000, debug=True)
 
 
-    #webbrowser.open("http://127.0.0.1:5000/")
-    #app.run("127.0.0.1", port=5000, debug=True)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- models.py
+++ models.py
@@ -1,25 +1,41 @@
 from flask_sqlalchemy import SQLAlchemy
-from faker import Faker
+# from faker import Faker
 
 
 db = SQLAlchemy()
 
 
-    # Bank customer, can be managed by the big dawgs
+# if shit hits the fan, do db.create_all()
+
+
+
+# Bank customer, can be managed by the big dawgs
 class Customer(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)  # id = kundnummer
     city = db.Column(db.String(80), nullable=False)
+    ssn = db.Column(db.Integer, nullable=False, unique=True)
+    accounts = db.relationship("accounts", backref="account", lazy=True)
+
+
+
+    # Bank account for a customer
+class Account(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # id = kontonummer
     saldo = db.Column(db.Float, nullable=False)
+    customer = db.Column(db.Integer, db.ForeignKey("customer.id"))
 
 
 
 
+
+
+"""
+    # Fake data
 def seed_data():
     if Customer.query.count() <= 0:
-
         fake = Faker()
 
-        for _ in range(3):
+        for _ in range(1000):
             city = fake.city()
 
             saldo = fake.random_number(digits=6)
@@ -27,4 +43,5 @@ def seed_data():
             customer = Customer(city=city, saldo=saldo)
             db.session.add(customer)
 
-        db.session.commit() # Must be done in the end
+        db.session.commit()  # Must be done in the end
+"""
